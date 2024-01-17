b3c81dd6ce4b9bd0ebbd75671d2e5263fa31cd3b
Jaisaarathi <141999413+Jaisaarathi@users.noreply.github.com>
2024-01-18 02:28:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import eel
 import yfinance as yf
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import numpy as np
 
